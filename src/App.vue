e8e56f4c993551cbd534ba93748383d187f377ef
Daniel Pereira de Souza <daniel_mfn@hotmail.com>
2019-11-17 17:00:19 -0300

    Alterações na estrutura do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <v-app>
+        <p>Agape App</p>
+    </v-app>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+    export default {
+        name: 'App',
+        data: () => ({
+            //
+        }),
+    };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
