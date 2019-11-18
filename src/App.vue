aa0bb36c414ee67520008353b94f1a56bd7393a2
Daniel Pereira de Souza <daniel_mfn@hotmail.com>
2019-11-18 15:33:10 -0300

    Reestruturação do projeto.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
 <template>
     <v-app>
-        <p>Agape App</p>
+        <app-header />
+
+        <v-content>
+            <router-view />
+        </v-content>
+
+        <feedback />
     </v-app>
 </template>
 
 <script>
+    import apiConfig from './components/api/apiConfig';
+
+    import AppHeader from './components/header/AppHeader.vue';
+    import Feedback from './components/feedback/Feedback.vue';
+
     export default {
         name: 'App',
-        data: () => ({
-            //
-        }),
+        components: { AppHeader, Feedback },
+        mixins: [apiConfig],
+        created() {
+            this.createInterceptors();
+
+            if (window.localStorage.authToken && window.localStorage.userId) {
+                this.$store.commit('setAuthToken', window.localStorage.authToken);
+                this.$store.commit('setUserId', window.localStorage.userId);
+            } else {
+                this.$router.push('/');
+            }
+        },
     };
 </script>
